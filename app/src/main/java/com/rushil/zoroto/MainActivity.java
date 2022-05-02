364399d1b30a7dc043569f2e365a9158743d7f18
Rushil Kakadiya <rushilpatel11011@gmail.com>
2022-05-02 18:51:07 +0530

    Zoro.to application for Android

---
+++ app/src/main/java/com/rushil/zoroto/MainActivity.java
@@ -0,0 +1,67 @@
+package com.rushil.zoroto;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new FullScreenClient(MainActivity.this){
+            @Override
+            public void onHideCustomView() {
+                hideFullScreen(webView);
+            }
+
+            @Override
+            public Bitmap getDefaultVideoPoster() {
+                return videoBitmap();
+            }
+
+            @Override
+            public void onShowCustomView(View view, CustomViewCallback callback) {
+                showFullScreen(view,callback);
+            }
+        });
+
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl("https://zoro.to/home");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setPluginState(WebSettings.PluginState.ON_DEMAND);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+
+}
